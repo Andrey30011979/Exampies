e2019364aa68a298bf7dc59e25513c1ac1458eb4
Andrey Marymov <marymovandrey078115@gmail.com>
2022-09-05 14:14:15 +0300

    Example 13

--- Example013_RecursionAlgoritm/Program.cs
+++ Example013_RecursionAlgoritm/Program.cs
@@ -40,7 +40,16 @@ FillArray(matrix);
 Console.WriteLine();
 PrintArray(matrix);
 */
-double Factorial(int n)//int Factorial(int n)
+/* 
+//Factorial
+//5i = 5*4*3*2*1
+//     5*4i
+//       4*3i
+//         3*2i
+//           2*1i
+double Factorial(int n)
+//int Factorial(int n)
+//1i = 1
 {
     if (n == 1) return 1;
     else return n*Factorial(n-1);
@@ -49,4 +58,21 @@ double Factorial(int n)//int Factorial(int n)
 for (int i = 1; i < 40; i++)
 {
 Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+*/
+// Fibonacci
+// 1 1 2 3 5 8 13 21 34 ....
+// fn = fn-1 + fn-2
+// f(1) = 1 
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+double Fibonacci(int n) //int Fibonacci(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+    for (int i = 1; i < 50; i++) //for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+    //Console.WriteLine(Fibonacci(i));
 }
